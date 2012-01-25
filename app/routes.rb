592f2a7ac1107de7d0c463a5dadfa28e5f3cd35d
Steven Klise <steven@stevenklise.com>
2012-01-24 19:26:18 -0500

    Add Javascript libraries

--- app/routes.rb
+++ app/routes.rb
@@ -4,13 +4,11 @@ end
 
 # Use Sass with `views/style.scss` as your stylesheet
 # To use, delete `public/stylehseets/style.css`
-# get '/stylesheets/style.css' do
-  # scss :style
-# end
+get '/css/style.css' do
+  scss :style
+end
 
-#   404
-#---------------------------------------
-
+# 404 Error
 not_found do
   erb :notfound
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
-require 'rubygems'
 require './index'
 run Sinatra::Application
